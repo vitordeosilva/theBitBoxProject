56b71d88fb79ab26d08e449b1872b5a711e177b0
unknown <vitor.de.o.silva@gmail.com>
2019-04-16 22:13:03 -0300

    Added server base + maven compiled project

---
+++ Server/src/main/java/server/HelloController.java
@@ -0,0 +1,13 @@
+package server;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Teste do servidor BitBox!";
+    }
+}
