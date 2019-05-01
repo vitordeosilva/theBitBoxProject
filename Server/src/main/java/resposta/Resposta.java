be81cc17c524ca1cbb2d8e2c813ac795b3213a19
unknown <vitor.de.o.silva@gmail.com>
2019-05-01 11:49:56 -0300

    resposta de login adicionada

--- Server/src/main/java/resposta/Resposta.java
+++ Server/src/main/java/resposta/Resposta.java
@@ -5,7 +5,7 @@ public class Resposta {
 	private int erro;
 	private String mensagem;
 	
-	Resposta (String mensagem, int erro) {
+	public Resposta (String mensagem, int erro) {
 		this.erro = erro;
 		this.mensagem = mensagem;
 	}
