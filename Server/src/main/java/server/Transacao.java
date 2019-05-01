8485b8a858d8a6a717ba0c9be15e20ddeedbe9c8
unknown <vitor.de.o.silva@gmail.com>
2019-05-01 15:54:54 -0300

    mudado de Id para ID

--- Server/src/main/java/server/Transacao.java
+++ Server/src/main/java/server/Transacao.java
@@ -2,7 +2,7 @@ package server;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.ID;
+import javax.persistence.Id;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
