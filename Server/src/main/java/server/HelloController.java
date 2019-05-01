ea9bcf08686be6940fce4a069fdde90d43e0974d
unknown <vitor.de.o.silva@gmail.com>
2019-05-01 12:09:10 -0300

    nova transacao com id automatico

--- Server/src/main/java/server/HelloController.java
+++ Server/src/main/java/server/HelloController.java
@@ -56,7 +56,7 @@ public class HelloController {
 
 	@PostMapping("/transacoes")
 	public ResponseEntity newTransacao(@RequestBody Transacao transacao) {
-		if (transacao.id != 0)
+		if (transacao.getId != 0)
 			return ResponseEntity.ok(new Resposta("ERROR - ID must be 0", 1));
 		return ResponseEntity.ok(transacaoRepository.save(transacao));
 	}
