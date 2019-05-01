0c55b3ed92fd71ba4549fe378cafe40fc882ef1c
unknown <vitor.de.o.silva@gmail.com>
2019-05-01 12:39:46 -0300

    resposta de login adicionada

--- Server/src/main/java/blockio/BlockIO.java
+++ Server/src/main/java/blockio/BlockIO.java
@@ -22,8 +22,11 @@ public class BlockIO {
 		
 		
 		if (bio_resposta.status.equals("success")) {
-			return new float[] {(float) 1.0, (float) 1.0};
-		}/*
+			return new float[] {(float) 2.0, (float) 2.0};
+		}
+		return new float[] {(float) 1.0, (float) 1.0};
+		
+		/*
 			float saldo = (float)bio_resposta.corpo.get("available_balance");
 			float saldo_pendente = (float)bio_resposta.corpo.get("pending_received_balance");
 			return new float[] {saldo, saldo_pendente};
