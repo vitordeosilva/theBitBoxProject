378008a778337858262d73bcc241290d15abf3ad
unknown <vitor.de.o.silva@gmail.com>
2019-05-01 15:23:27 -0300

    adicionada resposta no get de transacao com id

---
+++ Server/src/main/java/resposta/TransacaoResposta.java
@@ -0,0 +1,40 @@
+package resposta;
+
+import server.Transacao;
+
+public class NovaTransacaoResposta extends Resposta {
+	private Long id;
+	private int estado;
+    private Long usuarioID;
+    private Long maquinaID;
+    private Long produtoID;
+	
+	public NovaTransacaoResposta(String mensagem, int erro, Transacao transacao) {
+		super(mensagem, erro);
+		this.id = transacao.getId();
+		this.estado = transacao.getEstado();
+		this.usuarioID = transacao.getUsuarioId();
+		this.maquinaID = transacao.getMaquinaId();
+		this.produtoID = transacao.getProdutoId();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public int getEstado() {
+		return estado;
+	}
+	
+	public Long getUsuarioID() {
+		return usuarioID;
+	}
+	
+	public Long getMaquinaID() {
+		return maquinaID;
+	}
+	
+	public Long getProdutoID() {
+		return produtoID;
+	}
+}
