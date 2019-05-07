872d97a145c341197f17323f45aaa570e29aee93
unknown <vitor.de.o.silva@gmail.com>
2019-05-07 19:08:18 -0300

    funcao de checar saldo agora recebe um endereco

--- Server/src/main/java/blockio/BlockIO.java
+++ Server/src/main/java/blockio/BlockIO.java
@@ -8,18 +8,18 @@ import org.apache.http.util.EntityUtils;
 import com.google.gson.Gson;
 
 public class BlockIO {
-	public static float[] getSaldo(String api_key) throws Exception {
+	String api_key = "6bef-475f-4d48-2370";
+	
+	public static float[] getSaldo(String address) throws Exception {
 		
 		CloseableHttpClient client = HttpClients.createDefault();
-		HttpGet get = new HttpGet("https://block.io/api/v2/get_balance/?api_key=" + api_key);
+		HttpGet get = new HttpGet("https://block.io/api/v2/get_balance/?api_key=" + api_key + "&address=" + address);
 		CloseableHttpResponse resposta = client.execute(get);
 		
 		Gson gson = new Gson();
 		String jsonText = EntityUtils.toString(resposta.getEntity());
 		BlockIOResposta bio_resposta = gson.fromJson(jsonText, BlockIOResposta.class);
-		resposta.close();
-		
-		
+		resposta.close();		
 		
 		if (bio_resposta.status.equals("success")) {
 			float saldo = Float.parseFloat((String) bio_resposta.data.get("available_balance"));
